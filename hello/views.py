9a43600d15afdef6cd5698eb77a2c94161c05b4e
Armaan Goel <armaan.goel@berkeley.edu>
2020-07-18 21:48:00 -0700

    change state response for albert :)

--- hello/views.py
+++ hello/views.py
@@ -117,10 +117,7 @@ def getStateJson(id, user_id):
         # users = [u for u in rds.Huddle.list_users(id, huddle_id)]
 
         for u in rds.Huddle.list_users(id, huddle_id):
-             response['users'].push({
-                key: u,
-                value: huddle_id
-            }); 
+            response['users'][u] = huddle_id
 
         response['rooms'] += [{"id" : huddle_id, "users": users}]
 

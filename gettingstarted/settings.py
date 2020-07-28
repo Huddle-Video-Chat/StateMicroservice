62b2ea1236562d293ae5df893ea162ad18dd6cca
Zaiwen Gong <zgong@zaiwens-mbp.lan>
2020-07-28 15:59:40 -0400

    white

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "CHANGE_ME!!!! (P.S. the SECRET_KEY environment variable will be us
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["http://localhost:3000", "https://video-app-7307-dev.twil.io/","https://video-app-6833-dev.twil.io", "https://video-app-9567-dev.twil.io?passcode=5781279567, https://video-app-4043-dev.twil.io?passcode=3472804043"]
+ALLOWED_HOSTS = ["http://localhost:3000", "https://video-app-7307-dev.twil.io/","https://video-app-6833-dev.twil.io", "https://video-app-4043-dev.twil.io?passcode=3472804043"]
 
 
 # Application definition

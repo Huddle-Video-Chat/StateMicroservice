d4304e7cf28edf22bf740789d7912e7074f70ec4
Victoria Li <victoria.li@berkeley.edu>
2020-07-30 20:17:39 -0700

    wl

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = "CHANGE_ME!!!! (P.S. the SECRET_KEY environment variable will be us
 DEBUG = True
 
 ALLOWED_HOSTS = ["http://localhost:3000", "https://video-app-2545-dev.twil.io/","https://video-app-6833-dev.twil.io", "https://video-app-1525-dev.twil.io?passcode=3024591525", "https://video-app-2644-dev.twil.io", "http://localhost:3000", "https://video-app-2372-dev.twil.io/","https://video-app-6833-dev.twil.io", "https://video-app-9123-dev.twil.io?passcode=6255939123",
-"https://video-app-2378-dev.twil.io?passcode=9973962378", "https://video-app-4405-dev.twil.io?passcode=3919454405", "http://localhost:3000", "https://video-app-2372-dev.twil.io/","https://video-app-6833-dev.twil.io", "https://video-app-5680-dev.twil.io?passcode=5488025680"]
+"https://video-app-2378-dev.twil.io?passcode=9973962378", "https://video-app-4405-dev.twil.io?passcode=3919454405", "http://localhost:3000", "https://video-app-2372-dev.twil.io/","https://video-app-6833-dev.twil.io", "https://video-app-5680-dev.twil.io?passcode=5488025680", "https://video-app-3756-dev.twil.io"]
 
 # Application definition
 

bc19e4e342f6a25c3aa576edeb8476f793c51849
Zaiwen Gong <zgong@zaiwens-mbp.lan>
2020-08-23 23:29:38 -0400

    wl albert

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -27,11 +27,11 @@ SECRET_KEY = "CHANGE_ME!!!! (P.S. the SECRET_KEY environment variable will be us
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALBERT_WHITELIST="https://video-app-0249-6380-dev.twil.io"
+ALBERT_WHITELIST="https://video-app-3601-9145-dev.twil.io"
 VICKY_WHITELIST=""
+ARMAAN_WHITELIST=""
 
-ALLOWED_HOSTS=["http://localhost:3000", "https://video-app-9991-dev.twil.io","https://video-app-6833-dev.twil.io", "https://video-app-3514-dev.twil.io", "https://video-app-6362-6846-dev.twil.io",
-ALBERT_WHITELIST]
+ALLOWED_HOSTS=["http://localhost:3000" , ALBERT_WHITELIST, VICKY_WHITELIST, ARMAAN_WHITELIST]
 # Application definition
 
 INSTALLED_APPS = [

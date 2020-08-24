e069e2e0c889583fd6f86da2b5fcf11adcd10d31
Victoria Li <victoria.li@berkeley.edu>
2020-08-23 20:45:38 -0700

    white

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = "CHANGE_ME!!!! (P.S. the SECRET_KEY environment variable will be us
 DEBUG = True
 
 ALBERT_WHITELIST="https://video-app-3601-9145-dev.twil.io"
-VICKY_WHITELIST="https://video-app-5695-7270-dev.twil.io"
+VICKY_WHITELIST="https://video-app-2408-3213-dev.twil.io"
 ARMAAN_WHITELIST=""
 
 ALLOWED_HOSTS=["http://localhost:3000" , ALBERT_WHITELIST, VICKY_WHITELIST, ARMAAN_WHITELIST]
